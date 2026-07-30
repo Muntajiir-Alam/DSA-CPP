1c3e7af6959b63f21cc4f270ce8e1bbc2d3f079f
Muntajir Alam <munatjiralam20074@gamil.com>
2026-07-30 20:41:38 +0530

    add repo in github

---
+++ time.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    // Your code here
+    
+    return 0;
+}
