6cc87632d28c8f121d00bb219eda5249d05aeeae
Muntajir Alam <munatjiralam20074@gamil.com>
2026-08-04 15:28:39 +0530

    add function to check if an array is sorted and rotated

---
+++ array/checkShortedAndRoted.cpp
@@ -0,0 +1,28 @@
+// leetcode (1752)
+#include <iostream>
+#include <vector>
+using namespace std;
+bool check(vector<int> &nums)
+{
+    int count = 0;
+    int n = nums.size();
+    for (int i = 1; i < n; i++)
+    {
+        if (nums[i - 1] > nums[i] )
+        {
+            count++;
+        }
+    }
+    if (nums[n - 1] > nums[0]) count++;
+
+    if (count > 1){
+        return false;
+    }
+    return true;
+};
+
+int main()
+{
+    vector<int> arr = {3, 4, 5, 1, 2};
+    cout << check(arr) << endl;
+}
