d57bfeecd82abf2cf7d00c7f4325ef5be3354a0b
Muntajir Alam <muntajiralam20074@gmail.com>
2026-08-27 01:41:36 +0530

    add function to find the first unique character in a string

---
+++ string/firstUniqChar.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+#include <climits>
+#include <map>
+using namespace std;
+int firstUniqChar(string s)
+{
+    map<char, int> mp;
+
+    for (char c : s)
+        mp[c]++;
+
+    for (int i = 0; i < s.length(); i++)
+    {
+        if (mp[s[i]] == 1)
+            return i;
+    }
+    return -1;
+}
+
+int main()
+{
+    string s = "leetcode";
+    cout << firstUniqChar(s) << endl;
+}
