5fac8cd34d06c3b9c09669108886f8609f4f56ee
Muntajir Alam <muntajiralam20074@gmail.com>
2026-09-21 12:42:47 +0530

    refactor TreeNode class and update buildTree function for improved clarity

--- trees/preorderSeq.cpp
+++ trees/preorderSeq.cpp
@@ -3,36 +3,68 @@
 #include <climits>
 using namespace std;
 
-class Node
+class TreeNode
 {
 public:
-    int data;
-    Node *left;
-    Node *right;
-    Node(int val)
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int val)
     {
-        data = val;
+        this->val = val;
         left = right = NULL;
     }
 };
 static int idx = -1;
 
-Node *buildTree(vector<int> preorder)
+TreeNode *buildTree(vector<int> &preorder)
 {
     idx++;
     if (preorder[idx] == -1)
         return NULL;
-    
-    Node *root = new Node(preorder[idx]);
+
+    TreeNode *root = new TreeNode(preorder[idx]);
 
     root->left = buildTree(preorder);
     root->right = buildTree(preorder);
 
     return root;
 }
+
+// ────────────────── Traversals ───────────────────────
+
+void preorder(TreeNode *root)
+{
+    if (root == NULL)
+        return;
+    cout << root->val << " ";
+    preorder(root->left);
+    preorder(root->right);
+}
+void inorder(TreeNode *root)
+{
+    if (root == NULL)
+        return;
+    inorder(root->left);
+    cout << root->val << " ";
+    inorder(root->right);
+}
+
+void postorder(TreeNode *root)
+{
+    if (root == NULL)
+        return;
+    postorder(root->left);
+    postorder(root->right);
+    cout << root->val << " ";
+}
+
 int main()
 {
     vector<int> nums = {1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1};
-    Node *root = buildTree(nums);
+    TreeNode *root = buildTree(nums);
     cout << "Tree built from preorder sequence!" << endl;
+    // preorder(root);
+    // inorder(root);
+    postorder(root);
 }
